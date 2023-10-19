c0f4a47858f597d757e7fb5b16eb0172072855ab
rolf widenfelt <rolfvw@gmail.com>
2023-10-19 15:16:31 -0700

    P5 sketch here

---
+++ sketch.js
@@ -0,0 +1,46 @@
+// basic_socket_demo - for CP2 Fall 2023
+//
+// this "listens" for data that is forwarded by a node JS server.
+// it uses socket.io, a JavaScript library.
+//
+// author: Rolf Widenfelt
+
+
+let RAD = 50;
+
+let global_hue = 100;
+
+var socket = io();
+
+socket.on('data', function(data){
+
+  //print(data);
+  global_hue = data;  // can use parseInt(data) if this doesn't work
+
+});
+
+
+function setup() {
+  createCanvas(340, 340);
+  textAlign(CENTER);
+}
+
+function draw() {
+  colorMode(RGB);
+  background(220);
+  colorMode(HSB);
+
+  let cx = width/2;
+  let cy = height/2;
+  
+  // draw a colored circle with a text value in it
+  noStroke();
+  fill(global_hue, 100, 100);
+  ellipse(cx, cy, RAD*2);
+  
+  stroke('black');
+  noFill();
+  text(str(global_hue), cx, cy);
+}
+
+
